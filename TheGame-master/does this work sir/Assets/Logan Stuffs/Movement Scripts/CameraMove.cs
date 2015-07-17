05a5f85e8677d58ba554e60b079deceeaf8d47e5
Hypno-cube <logan.c.pettit@gmail.com>
2015-07-17 09:08:08 -0500

    I hope I don't Fuck this one up
    I really hope

---
+++ TheGame-master/does this work sir/Assets/Logan Stuffs/Movement Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	
+	public Transform Target;
+	public Transform Cam;
+	public float cany = 2f;
+	public float canx;
+	public float canz;
+
+	
+	
+		// Update is called once per frame
+	void Update () 
+	{
+		Cam.position = Target.position + new Vector3 (canx, cany, canz);
+	}
+}
---
+++ TheGame-master/does this work sir/Assets/Logan Stuffs/Movement Scripts/walk.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walk : MonoBehaviour {
+	Animator anim;
+	public float MouseSensitivity = 5f;
+	float vertRot;
+	public float UpDownRange = 60.0f;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{/*
+		float rotLeftRight = Input.GetAxis ("Mouse X") * MouseSensitivity;
+		transform.Rotate (0, rotLeftRight, 0);
+
+		vertRot -= Input.GetAxis ("Mouse Y") * MouseSensitivity;
+		vertRot = Mathf.Clamp (vertRot, -UpDownRange, UpDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler (vertRot , 0, 0);
+*/
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", move);
+
+		float Wmove = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("LRSpeed", Wmove);
+
+	
+	}
+}
