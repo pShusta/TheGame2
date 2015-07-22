e10fa3606b289259d3fceb6c6c1ea44d98ff6333
Phillip Shusta <pshusta@gmail.com>
2015-07-22 10:42:00 -0500

    More Title Screen Anim
    Got the start of buttons now.

---
+++ TheGame-master/does this work sir/Assets/Scripts/UI/MainMenu/TitleScreenButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenButton : MonoBehaviour {
+public GameObject Crate;
+public GameObject Words;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnClick(){
+		Debug.Log ("Click");
+	}
+}
